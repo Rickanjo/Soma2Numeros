6e5654cb8a6dc3fd4ea271c14720c5b2d09fe3ab
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-04-01 14:54:55 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//Leia 2 números inteiros e exiba o resultado da soma
+
+int num1;
+int num2;
+int Resultado;
+
+
+Console.WriteLine("----Calculo de Somas----\n");
+
+Console.Write("Digite seu primeiro valor:");
+num1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Digite seu segundo valor:");
+num2 = int.Parse(Console.ReadLine()!);
+
+Resultado = num1 + num2;
+
+Console.WriteLine($"A soma entre {num1} e {num2} é igual a {Resultado}");
+Console.WriteLine();
